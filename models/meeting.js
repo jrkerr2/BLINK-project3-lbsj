fa0bad597a2e4cd25716edc4292cd2605c105f05
jrkerr2 <johnrkerr@hotmail.com>
2018-11-09 13:57:41 -0500

    init content1

---
+++ models/meeting.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const meetingSchema = new Schema({
+
+// MEETING SCHEMA HERE
+    
+//   title: { type: String, required: true },
+//   author: { type: String, required: true },
+//   synopsis: String,
+//   date: { type: Date, default: Date.now }
+});
+
+const Book = mongoose.model("Meeting", meetingSchema);
+
+module.exports = Meeting;
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static assets for Heroku
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
-// Define API routes here
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blink");
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Server NOW on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
