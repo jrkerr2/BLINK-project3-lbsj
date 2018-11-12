fa3842e92f7ea05948fa4c4b124dc16e7a754394
jrkerr2 <johnrkerr@hotmail.com>
2018-11-12 15:55:22 -0500

    heroku test for mlab

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Blink database on Mongo
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blink");
+var dbCon = process.env.MONGODB_URI || "mongodb://root:root@192.168.99.100/blink?authSource=admin"
 
-var dbCon = "mongodb://root:root@192.168.99.100/blink?authSource=admin";
+// var dbCon = "mongodb://root:root@192.168.99.100/blink?authSource=admin";
 mongoose.connect(dbCon, { useNewUrlParser: true }, function(error) {
   if (error) {
     console.log("Database _BLINK_ Error:", error);
