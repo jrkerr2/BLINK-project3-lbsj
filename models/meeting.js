b4cb6de9d8c8083a288ca4b7c4241212c275fae0
jrkerr2 <johnrkerr@hotmail.com>
2018-11-12 12:22:47 -0500

    manually adding Shauna's schema merge to meeting.js

--- models/meeting.js
+++ models/meeting.js
@@ -9,8 +9,22 @@ const meetingSchema = new Schema({
 //   synopsis: String,
 //   date: { type: Date, default: Date.now }
 
+// John's TEST SCHEMA
     nana: { type: String, required: true },
     john: { type: String, required: true },
+
+// Shauna's initial SCHEMA
+    // meeting_id: { type: String, unique: true },
+    // meeting_name: { type: String },
+    // objective: [{ type: String }],
+    // acceptance_criteria: { type: String, },
+    // resolution: { type: String },
+    // notes: [{ type: String }], 
+    // attendees: [{ type: String, array: Array }],
+    // cost: { type: Number },
+    // duration: [{ type: Number }],
+    // rating: [{ type: Number }],
+    // role: [{ type: String }]
 
 });
 
