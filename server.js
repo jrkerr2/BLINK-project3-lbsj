19f7e365b33a364669e1672050e33d6881a1e1cf
jrkerr2 <johnrkerr@hotmail.com>
2018-11-17 07:00:10 -0500

    clean-up of server.js

--- server.js
+++ server.js
@@ -14,28 +14,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 };
 
-// *** IGNORE -- PUSHER PoC ***
-// // Pusher connect
-// var pusher = new Pusher({
-//   appId: '648683',
-//   key: 'c785fb8623f55789d46b',
-//   secret: 'a21fe0f432e225905f86',
-//   cluster: 'us2',
-//   encrypted: true
-// });
-
-// *** IGNORE -- PUSHER PoC ***
-// pusher.trigger('jk-channel', 'jk-event', {
-//   "message": "hello, Nana!",
-//   "name": "stuff",
-//   "channel": "ON home demo"
-
-// });
-
 // Add routes, both API and view
 app.use(routes);
 
-// Mac use 
 // Connect to the Blink database on Mongo
 var dbCon = process.env.MONGODB_URI || "mongodb://root:root@192.168.99.100/blink?authSource=admin"
 
